8ecfb359105bf342ba716b833434a4a2f14a8fb9
krisjin <krisibm@163.com>
2014-02-04 07:07:54 +0800

    添加评论、拦截器

--- src/main/java/net/eleword/blog/entity/Comment.java
+++ src/main/java/net/eleword/blog/entity/Comment.java
@@ -1,12 +1,32 @@
 package net.eleword.blog.entity;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * TODO 此处填写 class 信息
  * @author krisjin (mailto:krisjin86@163.com)
  * @date  2014-1-26上午11:13:51
  */
+@Entity
+@Table
+public class Comment extends IdEntity implements Serializable {
 
-public class Comment {
-
+	private static final long serialVersionUID = -3374963390660032076L;
+	
+	private String commentUser;
+	
+	private String commentContent;
+	
+	private Date  commentDate;
+	
+	private String email;
+	
+	private long articleId;
+	
+	
 }
 
---
+++ src/main/java/net/eleword/blog/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,19 @@
+
+package net.eleword.blog.interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+/**
+ * TODO 此处填写 class 信息
+ * @author krisjin (mailto:krisjin86@163.com)
+ * @date 2014-2-4上午7:05:46
+ */
+public class AuthorizationInterceptor extends AbstractInterceptor  {
+
+	public String intercept(ActionInvocation invocation) throws Exception {
+		return null;
+	}
+
+}
+
