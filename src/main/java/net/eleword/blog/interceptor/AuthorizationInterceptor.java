77ce986455caa282b816ae800e1248a84a52e42f
krisjin <krisibm@163.com>
2014-02-08 17:09:25 +0800

    修改配置文件

--- src/main/java/net/eleword/blog/interceptor/AuthorizationInterceptor.java
+++ src/main/java/net/eleword/blog/interceptor/AuthorizationInterceptor.java
@@ -1,6 +1,8 @@
 
 package net.eleword.blog.interceptor;
 
+import org.springframework.stereotype.Controller;
+
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
@@ -9,10 +11,16 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
  * @author krisjin (mailto:krisjin86@163.com)
  * @date 2014-2-4上午7:05:46
  */
+@Controller("loginInter")
 public class AuthorizationInterceptor extends AbstractInterceptor  {
 
 	public String intercept(ActionInvocation invocation) throws Exception {
-		return null;
+		
+		if(invocation.getInvocationContext().getSession().get("userId")==null){
+			return "";
+		}
+		
+		return invocation.invoke();
 	}
 
 }
